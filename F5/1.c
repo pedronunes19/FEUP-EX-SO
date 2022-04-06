0fc9969bb5f34b9dc9ccfd19efea68c3c644ae98
pedronunes19 <pedromiguelmnunes@gmail.com>
2022-04-06 18:33:24 +0100

    f5

---
+++ F5/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[]) {
+    /* fork a child process */
+    fork();
+    /* fork another child process */
+    fork();
+    /* and fork another */
+    fork();
+    printf("id: %d\n", getpid());
+    return EXIT_SUCCESS;
+}
